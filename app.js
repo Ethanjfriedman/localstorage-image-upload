05c3bee5ef2a79e70142bdf01e353bafce8d3506
Ethan Friedman <ethanjfriedman@gmail.com>
2016-10-07 22:47:34 -0500

    added in clear form values on submission + recall button to view prior submission

--- app.js
+++ app.js
@@ -14,13 +14,22 @@ document.addEventListener('DOMContentLoaded', () => {
       if (img && name && description) {
         count++;
         const imgURL = window.URL.createObjectURL(img);
-        const imgDisp = document.querySelector('img');
-        imgDisp.style.display = 'block';
-        imgDisp.src = imgURL;
-        h5.innerHTML = 'Your moment:';
         localStorage.setItem(`${count}-moment-image`, imgURL);
-        localStorage.setItem(`${count}-moment--name`, name);
-        localStorage.setItem(`${count}-moment-description`, description);
+        localStorage.setItem(`Name: ${count}-moment-name`, name);
+        localStorage.setItem(`Description: ${count}-moment-description`, description);
+        document.querySelectorAll('input').forEach(el => {
+          el.value = '';
+          el.files = null;
+        });
+        document.querySelector('label').innerHTML = 'Store another moment!'
+        document.querySelector('#review').addEventListener('click', () => {
+          event.preventDefault();
+          const imgDisp = document.querySelector('img');
+          imgDisp.src = localStorage.getItem(`${count}-moment-image`);
+          document.querySelector('h3').innerHTML = localStorage.getItem(`${count}-moment-name`);
+          document.querySelector('p').innerHTML = localStorage.getItem(`${count}-moment-description`);
+          document.querySelector('#review-div').style.display = 'block';
+        });
       } else {
         h5.innerHTML = 'please enter a name, description, and select an image to upload';
       }
